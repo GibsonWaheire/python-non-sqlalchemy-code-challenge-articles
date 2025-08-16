0539ee94ff2faaece6e5157f1c700b24d5c9292e
Gibson Waheire Giteru <pwriter455@gmail.com>
2025-08-16 10:25:16 +0300

    Enhanced comments in many_to_many.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -7,18 +7,18 @@ class Article:
         self._title = title
         Article.all.append(self)
     
-    @property
+    @property  
     def title(self):
-        return self._title
+        return self._title # returns the title of the article
     
     @title.setter
     def title(self, value):
         if hasattr(self, '_title'):
-            pass
+            pass # does not allow the title to be changed
     
     @property
     def author(self):
-        return self._author
+        return self._author # returns the author of the article
     
     @author.setter
     def author(self, value):
@@ -27,86 +27,86 @@ class Article:
     
     @property
     def magazine(self):
-        return self._magazine
+        return self._magazine # returns the magazine of the article
     
     @magazine.setter
     def magazine(self, value):
         if isinstance(value, Magazine):
-            self._magazine = value
+            self._magazine = value # allows the magazine to be changed
         
 class Author:
     def __init__(self, name):
-        self._name = name
+        self._name = name # initializes the name of the author
     
-    @property
+    @property # getter method
     def name(self):
-        return self._name
+        return self._name # returns the name of the author
     
-    @name.setter
+    @name.setter # setter method
     def name(self, value):
-        if hasattr(self, '_name'):
-            pass
+        if hasattr(self, '_name'): # checks if the name attribute exists
+            pass # does not allow the name to be changed
 
     def articles(self):
-        return [article for article in Article.all if article.author == self]
+        return [article for article in Article.all if article.author == self] # returns a list of all the articles the author has written
 
     def magazines(self):
-        magazines = []
+        magazines = [] # initializes an empty list to store the magazines
         for article in self.articles():
-            if article.magazine not in magazines:
-                magazines.append(article.magazine)
+            if article.magazine not in magazines: # checks if the magazine is not already in the list
+                magazines.append(article.magazine) # adds the magazine to the list
         return magazines
 
     def add_article(self, magazine, title):
-        return Article(self, magazine, title)
+        return Article(self, magazine, title) # creates a new article and returns it
 
     def topic_areas(self):
         if not self.articles():
             return None
-        categories = []
+        categories = [] # initializes an empty list to store the categories
         for magazine in self.magazines():
-            if magazine.category not in categories:
-                categories.append(magazine.category)
-        return categories
+            if magazine.category not in categories: # checks if the category is not already in the list
+                categories.append(magazine.category) # adds the category to the list
+        return categories # returns the list of categories
 
 class Magazine:
     def __init__(self, name, category):
-        self._name = name
-        self._category = category
+        self._name = name # initializes the name of the magazine
+        self._category = category # initializes the category of the magazine
     
     @property
     def name(self):
-        return self._name
+        return self._name # returns the name of the magazine
     
     @name.setter
     def name(self, value):
         if isinstance(value, str) and 2 <= len(value) <= 16:
-            self._name = value
+            self._name = value # allows the name to be changed
     
     @property
     def category(self):
-        return self._category
+        return self._category # returns the category of the magazine
     
     @category.setter
     def category(self, value):
         if isinstance(value, str) and len(value) > 0:
-            self._category = value
+            self._category = value # allows the category to be changed
 
     def articles(self):
-        return [article for article in Article.all if article.magazine == self]
+        return [article for article in Article.all if article.magazine == self] # returns a list of all the articles the magazine has published
 
     def contributors(self):
-        authors = []
+        authors = [] # initializes an empty list to store the authors
         for article in self.articles():
-            if article.author not in authors:
-                authors.append(article.author)
-        return authors
+            if article.author not in authors: # checks if the author is not already in the list
+                authors.append(article.author) # adds the author to the list
+        return authors # returns the list of authors
 
     def article_titles(self):
-        articles = self.articles()
+        articles = self.articles() # returns a list of all the articles the magazine has published
         if not articles:
-            return None
-        return [article.title for article in articles]
+            return None # returns None if the magazine has no articles
+        return [article.title for article in articles] # returns a list of the titles of all the articles the magazine has published
 
     def contributing_authors(self):
         author_counts = {}
@@ -115,4 +115,4 @@ class Magazine:
             author_counts[author] = author_counts.get(author, 0) + 1
         
         contributing = [author for author, count in author_counts.items() if count > 2]
-        return contributing if contributing else None
+        return contributing if contributing else None # returns the list of authors who have written more than 2 articles for the magazine
